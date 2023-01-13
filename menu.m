551feffd2a86849b7b640789aae98bf0988a0e16
SX-7 <92227810+SX-7@users.noreply.github.com>
2023-01-13 19:57:29 +0100

    Got rid of dysfunctional cubic interp
    Cubic interpolation only works for 2d plots
    Also added few comments and explainers

--- menu.m
+++ menu.m
@@ -8,18 +8,19 @@ z = A(:,1);
 xvar = min(x) : (max(x)-min(x))/50 : max(x);
 yvar = min(y) : (max(y)-min(y))/50 : max(y);
 [Xi,Yi] = meshgrid( xvar, yvar );
-outc = griddata( x, y, z, Xi,Yi, 'cubic' );
-outl = griddata( x, y, z, Xi,Yi, 'linear' );
-outv = griddata( x, y, z, Xi,Yi, 'v4' ); % <-- so far the best
-outn = griddata( x, y, z, Xi,Yi, 'nearest' );
-outnn = griddata( x, y, z, Xi,Yi, 'natural' );
-figure; surf( outc ); colorbar; 
+%outc = griddata( x, y, z, Xi,Yi, 'cubic' ); % <-- Does NOT support 3d
+%plots
+outl = griddata( x, y, z, Xi,Yi, 'linear' ); % only C0 continuity
+outv = griddata( x, y, z, Xi,Yi, 'v4' ); % <-- so far the best, C2!
+outn = griddata( x, y, z, Xi,Yi, 'nearest' ); % no continuity, but safe
+outnn = griddata( x, y, z, Xi,Yi, 'natural' ); % only C1 continuity
+%figure; surf( outc ); colorbar; title("Cubic interpolation");
+%xlim([1,51]); ylim([1,51]); % hardcoded to the current values
+figure; surf( outl ); colorbar; title("Linear interpolation");
 xlim([1,51]); ylim([1,51]); % hardcoded to the current values
-figure; surf( outl ); colorbar; 
+figure; surf( outv ); colorbar; title("V4 (biharmonic spline) interpolation");
 xlim([1,51]); ylim([1,51]); % hardcoded to the current values
-figure; surf( outv ); colorbar; 
+figure; surf( outn ); colorbar; title("Nearest interpolation");
 xlim([1,51]); ylim([1,51]); % hardcoded to the current values
-figure; surf( outn ); colorbar; 
-xlim([1,51]); ylim([1,51]); % hardcoded to the current values
-figure; surf( outnn ); colorbar; 
+figure; surf( outnn ); colorbar; title("Natural interpolation");
 xlim([1,51]); ylim([1,51]); % hardcoded to the current values
